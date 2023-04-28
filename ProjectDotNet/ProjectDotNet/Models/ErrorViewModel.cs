648a6ad0ca8b8c57989c82628609aee02b9bc857
coldoasis <jeremy.yap27@gmail.com>
2023-04-28 14:13:12 +0800

    tictactoe

--- ProjectDotNet/ProjectDotNet/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ProjectDotNet.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
